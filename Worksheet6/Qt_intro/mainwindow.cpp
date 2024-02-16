d125e6dd6f511b953ea5eed973bad6b5544bcf66
Tyler Channer <eeytc9@nottingham.ac.uk>
2024-02-16 09:48:42 +0000

    Worksheet 6 Exercise 2: Handling a button click

--- Worksheet6/Qt_intro/mainwindow.cpp
+++ Worksheet6/Qt_intro/mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Assuming you have buttons named pushButton1, pushButton2,  in your UI
+    connect(ui->pushButton, &QPushButton::released, this, &MainWindow::handleButton1);
+    connect(ui->pushButton_2, &QPushButton::released, this, &MainWindow::handleButton2);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::handleButton1() {
+    QMessageBox::information(this, "Button 1 Clicked", "You clicked button 1.");
+}
+
+void MainWindow::handleButton2() {
+    QMessageBox::information(this, "Button 2 Clicked", "You clicked button 2.");
+}
+
--- Worksheet6/Qt_intro/mainwindow.h
+++ Worksheet6/Qt_intro/mainwindow.h
@@ -2,22 +2,26 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox> // Include QMessageBox for showing messages
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT // Enable Qt's Meta-Object Compiler
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void handleButton1();
+    void handleButton2();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
 };
+
 #endif // MAINWINDOW_H

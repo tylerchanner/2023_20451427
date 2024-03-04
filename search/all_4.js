dfb47ade3da0d5dca556401484af04a638bc1143
tylerchanner <77684398+tylerchanner@users.noreply.github.com>
2024-03-04 14:39:09 +0000

    Deploying to gh-pages from @ tylerchanner/2023_20451427@3c3d7ea2baf64cabda5dd8a7d39eae9f607cfff7 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['getactor_11',['getActor',['../classModelPart.html#a3d845a3cb1d357a0c2f4ffb131e73116',1,'ModelPart']]],
   ['getcolor_12',['getColor',['../classOptionDialog.html#a9913b04ce34c705c692609ac85260c1d',1,'OptionDialog']]],
-  ['getcolourb_13',['getColourB',['../classModelPart.html#a2cae1e4fd951f1a5f62b93a7f9c68521',1,'ModelPart']]],
-  ['getcolourg_14',['getColourG',['../classModelPart.html#a3ddfad7df9c628e07f1f5e50a47d41d3',1,'ModelPart']]],
-  ['getcolourr_15',['getColourR',['../classModelPart.html#af2c1b405404cdb46d45a804838440e3a',1,'ModelPart']]],
+  ['getcolourb_13',['getColourB',['../classModelPart.html#a2e218138ad076a99db3c0ee2e82ea922',1,'ModelPart']]],
+  ['getcolourg_14',['getColourG',['../classModelPart.html#aaa8f00a3629526890007b5934807d354',1,'ModelPart']]],
+  ['getcolourr_15',['getColourR',['../classModelPart.html#aa974125427ff13c6d84417aa98db4115',1,'ModelPart']]],
   ['getgroupname_16',['getGroupName',['../classNewGroupDialog.html#a995bff4be50405cb73bdb088518e97d4',1,'NewGroupDialog']]],
   ['getitem_17',['getItem',['../classModelPartList.html#a90a142ded846cf21e90ad7b2d22be9ba',1,'ModelPartList']]],
   ['getname_18',['getName',['../classOptionDialog.html#ace0af27c027082ead61b1e13dc32730e',1,'OptionDialog']]],
